fdcb1d43bbe579ecd18f96107eb8615bf918f12b
Akshat Zala <akshatzala@gmail.com>
2020-10-02 14:15:14 +0530

    React.component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      monsters:[
+        {
+          name: 'Frankenstein',
+          id:'1'
+        },
+        {
+          name: 'Dracula',
+          id:2
+        },
+        {
+          name: 'Zombie',
+          id:3
+        },
+      ]
+    };
+  }
 
+  render(){
+    return (
+      <div className ="App">
+        {
+          this.state.monsters.map(monsters => (
+            <h1 key={monsters.id}>{monsters.name}</h1>
+          ))
+        }
+      </div>
+    );
+  }
+}
 export default App;
