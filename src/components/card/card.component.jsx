6b874589a943ebb238252ce37de7569869aa4484
Akshat Zala <akshatzala@gmail.com>
2020-10-02 17:17:43 +0530

    Everthing except search bar

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,6 +4,8 @@ import './card.styles.css';
 
 export const Card = (props) => (
     <div className="card-container">
-        <h1>{props.monsters.name}</h1>
+        <img alt="monster" src={`https://robohash.org/${props.monsters.id}?set=set2&size=180x180`}/>
+        <h2>{props.monsters.name}</h2>
+        <p>{props.monsters.email}</p>
     </div>
 )
